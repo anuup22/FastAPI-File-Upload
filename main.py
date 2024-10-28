e935358eafbe319f348870f3b60b02d0863e7430
Anup Gupta <anuup22.cs@gmail.com>
2024-10-28 18:08:33 +0530

    Step 2: Store uploaded file on the server

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, File, UploadFile
+import os
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 @app.post("/uploadfile/")
 async def upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    file_location = f"uploads/{file.filename}"
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
